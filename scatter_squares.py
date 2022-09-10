6880680e53a1a235589aeb164b510c0287836488
Roman <roman.matrosov.frontend@yandex.ru>
2022-09-10 16:46:18 +0300

    Scatter squares

---
+++ scatter_squares.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+x_values = range(200)
+y_values = []
+
+for val in x_values:
+    y_values.append(val * val)
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+
+ax.scatter(x_values, y_values, s=20)
+
+# Назначение заголовка диаграммы и меток осей
+ax.set_title("Square numbers", fontsize=18)
+ax.set_xlabel("Value", fontsize=16)
+ax.set_ylabel("Square of value", fontsize=16)
+
+ax.tick_params(axis="both", which="major", labelsize=12)
+
+ax.axis([0, 400, 0, 40000])
+
+plt.show()
