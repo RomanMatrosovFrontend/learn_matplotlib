27f111b5a29198750bff60e920ed17a34ce6002b
Roman <roman.matrosov.frontend@yandex.ru>
2022-09-11 00:45:14 +0300

    Save as image - plt.savefig()

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 
 x_values = range(200)
-y_values = []
-
-for val in x_values:
-    y_values.append(val * val)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, s=20)
+ax.scatter(x_values, y_values, s=20, c=y_values, cmap=plt.cm.autumn)
 
 # Назначение заголовка диаграммы и меток осей
 ax.set_title("Square numbers", fontsize=18)
@@ -20,4 +17,5 @@ ax.tick_params(axis="both", which="major", labelsize=12)
 
 ax.axis([0, 400, 0, 40000])
 
-plt.show()
+# plt.show()
+plt.savefig('scatter_squares.png', bbox_inches='tight')
